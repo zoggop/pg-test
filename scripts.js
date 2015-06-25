acfd251bde356ff8e62689b1434066beadb8756a
zoggop <zoggop@gmail.com>
2015-06-25 14:11:42 -0700

    put scripts.js in header, use cookies

--- scripts.js
+++ scripts.js
@@ -1,3 +1,32 @@
 // Your scripts go here.
 
-console.log('hello world!');
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+} 
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+var lightSheet = "{{site.baseurl}}/colorlight.css";
+var darkSheet = "{{site.baseurl}}/colordark.css";
+var currentSheet = darkSheet;
+var currentCookieSheet = getCookie("colorSheet");
+if (currentCookieSheet != "") { currentSheet = currentCookieSheet; }
+function swapColorSheet(){
+    var nextSheet = lightSheet;
+    if (currentSheet == lightSheet) { nextSheet = darkSheet; }
+    document.getElementById('colorstyle').setAttribute('href', nextSheet);
+    currentSheet = nextSheet;
+    setCookie("colorSheet", currentSheet, 30);
+}
