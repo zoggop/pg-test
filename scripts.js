98dac00697e0bac016358cd9ef5cc43038b93089
zoggop <zoggop@gmail.com>
2015-07-06 23:06:09 -0700

    hidden disqus

--- scripts.js
+++ scripts.js
@@ -32,3 +32,26 @@ if (currentCookieSheet != "") {
 		swapColorSheet();
 	}
 }
+     
+// Replace labnol with your disqus shortname
+var disqus_shortname = "thebluescreenofdeath";
+ 
+var disqus_loaded = false;
+ 
+// This is the function that will load Disqus comments on demand
+function disqus() {
+ 
+  if (!disqus_loaded)  {
+    
+    // This is to ensure that Disqus widget is loaded only once
+    disqus_loaded = true;
+    
+    var e = document.createElement("script");
+    e.type = "text/javascript";
+    e.async = true;
+    e.src = "//" + disqus_shortname + ".disqus.com/embed.js";
+    (document.getElementsByTagName("head")[0] ||
+     document.getElementsByTagName("body")[0])
+    .appendChild(e);
+  }
+} 
